5e88631e4f77fc66592b01d1f3676eb4dbcefb8a
kimbyoungseok <justmellow@naver.com>
2025-03-28 20:45:11 +0900

    Create CXGameModeBase.cpp

---
+++ CXGameModeBase.cpp
@@ -0,0 +1,90 @@
+// CXGameModeBase.cpp
+
+
+...
+
+FString ACXGameModeBase::GenerateSecretNumber()
+{
+	TArray<int32> Numbers;
+	for (int32 i = 1; i <= 9; ++i)
+	{
+		Numbers.Add(i);
+	}
+
+	FMath::RandInit(FDateTime::Now().GetTicks());
+	Numbers = Numbers.FilterByPredicate([](int32 Num) { return Num > 0; });
+	
+	FString Result;
+	for (int32 i = 0; i < 3; ++i)
+	{
+		int32 Index = FMath::RandRange(0, Numbers.Num() - 1);
+		Result.Append(FString::FromInt(Numbers[Index]));
+		Numbers.RemoveAt(Index);
+	}
+
+	return Result;
+}
+
+bool ACXGameModeBase::IsGuessNumberString(const FString& InNumberString)
+{
+	bool bCanPlay = false;
+
+	do {
+
+		if (InNumberString.Len() != 3)
+		{
+			break;
+		}
+
+		bool bIsUnique = true;
+		TSet<TCHAR> UniqueDigits;
+		for (TCHAR C : InNumberString)
+		{
+			if (FChar::IsDigit(C) == false || C == '0')
+			{
+				bIsUnique = false;
+				break;
+			}
+			
+			UniqueDigits.Add(C);
+		}
+
+		if (bIsUnique == false)
+		{
+			break;
+		}
+
+		bCanPlay = true;
+		
+	} while (false);	
+
+	return bCanPlay;
+}
+
+FString ACXGameModeBase::JudgeResult(const FString& InSecretNumberString, const FString& InGuessNumberString)
+{
+	int32 StrikeCount = 0, BallCount = 0;
+
+	for (int32 i = 0; i < 3; ++i)
+	{
+		if (InSecretNumberString[i] == InGuessNumberString[i])
+		{
+			StrikeCount++;
+		}
+		else 
+		{
+			FString PlayerGuessChar = FString::Printf(TEXT("%c"), InGuessNumberString[i]);
+			if (InSecretNumberString.Contains(PlayerGuessChar))
+			{
+				BallCount++;				
+			}
+		}
+	}
+
+	if (StrikeCount == 0 && BallCount == 0)
+	{
+		return TEXT("OUT");
+	}
+
+	return FString::Printf(TEXT("%dS%dB"), StrikeCount, BallCount);
+}
